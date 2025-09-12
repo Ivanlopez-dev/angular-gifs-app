7b2deac60e6afc319b62c1ee8a3a412a3202c0a7
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-12 14:03:03 +0200

    add: ✨ search page component & search gifs service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,10 +13,11 @@ export class GifService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal(true);
 
+  searchedGifs = signal<Gif[]>([]);
+
   constructor() {
     this.loadTrendingGifs();
     console.log('⚡ Servicio creado');
-
   }
 
   loadTrendingGifs() {
@@ -32,6 +33,21 @@ export class GifService {
         this.trendingGifsLoading.set(false);
         console.log({ gifs });
 
+      })
+  }
+
+  searchGifs(query: string) {
+    this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          q: query,
+          limit: 20,
+        }
+      }).subscribe((resp) => {
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        // this.searchedGifs.set(gifs)
+        console.log("Search: ", { gifs });
       })
   }
 }
