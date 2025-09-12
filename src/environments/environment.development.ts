ba8556230bfe764f7c2c95883956c23a06c81c6a
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-12 11:31:45 +0200

    update: 🛠️ environment constants

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   companyName: 'Gifs',
   companyName2: 'App',
-  companySlogan: 'Gestiona tus gifs'
+  companySlogan: 'Gestiona tus gifs',
 
   // ApiKeys
-
+  giphyApiKey: 'NhSj8FZfoeireWVpFSleQUDZ01fGKkvN',
   // URLS
-
+  giphyUrl: 'https://api.giphy.com/v1'
 };
