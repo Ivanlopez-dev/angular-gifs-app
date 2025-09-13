a1a5dc408e8c8e7a5a8f49eacf4d2c648c6b439b
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-13 17:05:48 +0200

    add: 👔 infinite scroll - state service, preserve scroll position

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,30 +1,37 @@
-import { Component, ElementRef, inject, viewChild } from '@angular/core';
-import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.service';
-
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
-  // imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService)
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  // Required when using implements AfterViewInit
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
     if (!scrollDiv) return;
 
-
     const scrollTop = scrollDiv.scrollTop; // -> How much User SCROLLED from top
     const clientHeight = scrollDiv.clientHeight; // -> // How much is the User VIEWPORT/VIEWPOINT's height
     const scrollHeight = scrollDiv.scrollHeight; // -> // Max height to the bottom
 
-    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight; // ->
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight; // -> Calculate if User scroll position is Bottom (Returns a boolean)
+    this.scrollStateService.trendingScrollState.set(scrollTop);
 
     if (isAtBottom) {
-      // Load the next 20 Gifs
+      // Load the next page (20 Gifs)
       this.gifService.loadTrendingGifs();
     }
   }
